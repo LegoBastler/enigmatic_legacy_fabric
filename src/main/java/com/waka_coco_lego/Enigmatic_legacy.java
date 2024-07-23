b84cba3dfb3b720ae66afd263d32f03e334520a4
LegoBastler <dennis.temschenko@gmail.com>
2024-07-23 22:27:52 +0200

    the merge

--- src/main/java/com/waka_coco_lego/Enigmatic_legacy.java
+++ src/main/java/com/waka_coco_lego/Enigmatic_legacy.java
@@ -5,13 +5,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Enigmatic_legacy_fabric implements ModInitializer {
+public class Enigmatic_legacy implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("enigmatic_legacy_fabric");
-
-	//THIS IS A TEST MESSAGE
+    public static final Logger LOGGER = LoggerFactory.getLogger("enigmatic_legacy");
 
 	@Override
 	public void onInitialize() {
--- src/main/java/com/waka_coco_lego/Enigmatic_legacyDataGenerator.java
+++ src/main/java/com/waka_coco_lego/Enigmatic_legacyDataGenerator.java
@@ -3,7 +3,7 @@ package com.waka_coco_lego;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class Enigmatic_legacy_fabricDataGenerator implements DataGeneratorEntrypoint {
+public class Enigmatic_legacyDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
