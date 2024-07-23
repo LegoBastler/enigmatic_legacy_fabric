8897fb5b0cadd52767fb59ca56c559f4614d3117
LegoBastler <dennis.temschenko@gmail.com>
2024-07-23 21:48:23 +0200

    #1

---
+++ src/client/java/com/waka_coco_lego/Enigmatic_legacy_fabricClient.java
@@ -0,0 +1,10 @@
+package com.waka_coco_lego;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Enigmatic_legacy_fabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/waka_coco_lego/Enigmatic_legacy_fabricDataGenerator.java
@@ -0,0 +1,11 @@
+package com.waka_coco_lego;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Enigmatic_legacy_fabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
