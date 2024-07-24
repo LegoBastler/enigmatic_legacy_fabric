57ab7bcea4de489cad66aa9f4a35715f35c3b24f
Cocogoat473 <lorenzkeim1@gmail.com>
2024-07-24 22:07:02 +0200

    ad

--- src/main/java/com/waka_coco_lego/enigmaticlegacy/items/Iron_Ring.java
+++ src/main/java/com/waka_coco_lego/enigmaticlegacy/items/Iron_Ring.java
@@ -1,7 +1,32 @@
 package com.waka_coco_lego.enigmaticlegacy.items;
 
-public class Iron_Ring {
+import com.google.common.collect.Multimap;
+import dev.emi.trinkets.api.SlotAttributes;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.TrinketItem;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
+import java.util.UUID;
 
+import static com.waka_coco_lego.enigmaticlegacy.Enigmatic_Legacy.MODID;
 
+public class Iron_Ring extends TrinketItem {
+
+    public Iron_Ring(Settings settings) {
+        super(settings);
+    }
+
+    public Multimap<EntityAttribute, EntityAttributeModifier> getModifiers(ItemStack stack, SlotReference slot, LivingEntity entity, UUID uuid) {
+        var modifiers = super.getModifiers(stack, slot, entity, uuid);
+        // +10% movement speed
+        modifiers.put(EntityAttributes.GENERIC_MOVEMENT_SPEED, new EntityAttributeModifier(new Identifier(MODID + ":movement_speed"), 0.1, EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+        // If the player has access to ring slots, this will give them an extra one
+        SlotAttributes.addSlotModifier(modifiers, "hand/ring", uuid, 1, EntityAttributeModifier.Operation.ADDITION);
+        return modifiers;
+    }
 }
